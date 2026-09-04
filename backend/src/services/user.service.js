9944f5de3afd6139b3a0520f1c96ef4df6ff8af0
Aline <alinernunes15@gmail.com>
2026-09-03 22:05:03 -0300

    feat(user): padroniza persistencia em users.json

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join(__dirname, "../data/user.json");
+const filePath = path.join(__dirname, "../data/users.json");
 
 
 function getUsers() {
